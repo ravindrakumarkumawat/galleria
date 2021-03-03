1734aac5b8625c33cd24f933bcf635c0e0fa2bda
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2021-03-03 12:14:48 +0530

    css is chaged based on column gap and column count

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -8,29 +8,29 @@ const Image = ({image, user, alt}) => {
     <div className='image-container' onClick={() => setLgShow(true)}>
       <img src={image.thumb} alt={alt} />
       <Modal
-      size="lg"
-      show={lgShow}
-      onHide={() => setLgShow(false)}
-      aria-labelledby="example-modal-sizes-title-lg"
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="example-modal-sizes-title-lg">
-        <div className='image-user'>        
-          <div>
-            <img src={user.profile_image.small} alt={user.first_name}/>
+        size="lg"
+        show={lgShow}
+        onHide={() => setLgShow(false)}
+        aria-labelledby="example-modal-sizes-title-lg"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="example-modal-sizes-title-lg">
+          <div className='image-user'>        
+            <div>
+              <img src={user.profile_image.small} alt={user.first_name}/>
+            </div>
+            <div>
+              <h1>{user.name} </h1>
+              <p className='followers'>Likes: {user.total_likes}</p>
+            </div>
           </div>
-          <div>
-          <h1>{user.name} </h1>
-          <p className='followers'>Likes: {user.total_likes}</p>
-          </div>
-    </div>
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <img src={image.small} alt={alt} width='100%' /> 
-      
-      </Modal.Body>
-    </Modal>
+          </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <img src={image.small} alt={alt} width='100%' /> 
+          
+          </Modal.Body>
+      </Modal>
     </div>
   )
 }
